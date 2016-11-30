bf036175b28dd3033d3ffa5ae50f2c521a235d40
Andres Farias <andres.farias@blueprintsit.cl>
2016-11-30 10:26:54 -0300

    !!!

---
+++ SemantikosKernelEJB/src/cl/minsal/semantikos/kernel/components/ispfetcher/CVSLoader.java
@@ -0,0 +1,11 @@
+package cl.minsal.semantikos.kernel.components.ispfetcher;
+
+/**
+ * @author Andrés Farías on 11/29/16.
+ */
+public class CVSLoader {
+
+    public static void main(String[] args) {
+        System.out.println("Test");
+    }
+}
