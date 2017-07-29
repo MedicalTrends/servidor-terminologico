c2ad058bff336bc897164ca181ac09fc31fce6f4
gpunucura <gustavopunucura@gmail.com>
2017-07-29 14:07:51 -0400

    Adaptando rama a nuevo ambiente Oracle

--- SemantikosWeb/src/cl/minsal/semantikos/concept/ConceptBean.java
+++ SemantikosWeb/src/cl/minsal/semantikos/concept/ConceptBean.java
@@ -402,17 +402,7 @@ public class ConceptBean implements Serializable {
 
     //Este método es responsable de a partir de un concepto SMTK y un término, devolver un concepto WEB con su FSN y su Preferida
     public ConceptSMTKWeb initConcept(ConceptSMTK concept, String term) {
-        ConceptSMTKWeb conceptWeb = new ConceptSMTKWeb(concept);
-
-        DescriptionWeb fsnDescription = new DescriptionWeb(conceptWeb, term, descriptionManager.getTypeFSN());
-        fsnDescription.setDescriptionId(" ");
-
-        DescriptionWeb favouriteDescription = new DescriptionWeb(conceptWeb, term, descriptionManager.getTypeFavorite());
-        favouriteDescription.setDescriptionId(" ");
-
-        for (DescriptionWeb description : new DescriptionWeb[]{favouriteDescription, fsnDescription}) {
-            conceptWeb.addDescriptionWeb(description);
-        }
+        ConceptSMTKWeb conceptWeb = new ConceptSMTKWeb(concept, term);
 
         fullyDefined = concept.isFullyDefined();
 
