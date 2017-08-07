2abde45574cc0a68b0518e9bb74ef5325f2410d8
gpunucura <gustavopunucura@gmail.com>
2017-08-07 15:01:53 -0400

    PoC refactorizando mappers dentro de DAOs

--- SemantikosKernelEJB/src/cl/minsal/semantikos/kernel/daos/InitFactoriesDAOImpl.java
+++ SemantikosKernelEJB/src/cl/minsal/semantikos/kernel/daos/InitFactoriesDAOImpl.java
@@ -489,4 +489,31 @@ public class InitFactoriesDAOImpl implements InitFactoriesDAO {
 
         return DataSourceFactory.getInstance();
     }
+
+    /**
+     * Este método es responsable de crear un HelperTable Record a partir de un objeto JSON.
+     *
+     * @param rs El objeto JSON a partir del cual se crea el objeto. El formato JSON será:
+     *                       <code>{"TableName":"helper_table_atc","records":[{"id":1,"codigo_atc":"atc1"}</code>
+     *
+     * @return Un objeto fresco de tipo <code>HelperTableRecord</code> creado a partir del objeto JSON.
+     *
+     * @throws IOException Arrojada si hay un problema.
+     */
+    public DescriptionType createDescriptionTypeFromResultSet(ResultSet rs) {
+
+        DescriptionType descriptionType = new DescriptionType();
+
+        try {
+            descriptionType.setId(rs.getLong("id"));
+            descriptionType.setName(rs.getString("name"));
+            descriptionType.setDescription(rs.getString("description"));
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return descriptionType;
+
+    }
 }
