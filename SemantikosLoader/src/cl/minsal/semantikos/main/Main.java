00d155de6fc4297e723087f486cd15825f842dbb
gpunucura <gustavopunucura@gmail.com>
2018-09-05 19:33:52 -0300

    deploy loader TFC para producción

--- SemantikosLoader/src/cl/minsal/semantikos/main/Main.java
+++ SemantikosLoader/src/cl/minsal/semantikos/main/Main.java
@@ -12,7 +12,7 @@ public class Main {
         SMTKLoader smtkLoader = new SMTKLoader();
 
         try {
-            smtkLoader.processMaster();
+            smtkLoader.process();
         } catch (Exception e) {
             e.printStackTrace();
         }
